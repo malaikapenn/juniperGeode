555132dab33dbbbd1349feb4f8cbbb3cf4020b2f
Malaika <57915549+malaikapenn@users.noreply.github.com>
2020-03-23 09:16:37 -0500

    Uploaded XCode project for plugin

---
+++ JuniperPluginXcode/JuniperPlugin/JuniperPlugin.h
@@ -0,0 +1,19 @@
+//
+//  JuniperPlugin.h
+//  JuniperPlugin
+//
+//  Created by Malaika Penn on 3/13/20.
+//  Copyright © 2020 Malaika Penn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JuniperPlugin.
+FOUNDATION_EXPORT double JuniperPluginVersionNumber;
+
+//! Project version string for JuniperPlugin.
+FOUNDATION_EXPORT const unsigned char JuniperPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JuniperPlugin/PublicHeader.h>
+
+
